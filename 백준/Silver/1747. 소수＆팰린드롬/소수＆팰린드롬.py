6bc6598a8aa53747ef562659d7f279b1f63886d5
rbflakt <152948102+rbflakt@users.noreply.github.com>
2025-01-15 17:37:04 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3520 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,25 @@
+def find():
+  N = int(input())
+  
+  def is_prime(n):
+    if n == 1:
+      return False
+    else:
+      for i in range(2,int(n**0.5)+1):
+        if n % i ==0:
+          return False
+    return True
+
+  def pal(n):
+    s = str(n)
+    if s == s[::-1]:
+      return True
+    else:
+      return False
+
+  for k in range(N,10**7):
+    if is_prime(k) and pal(k):
+      return k
+      break
+
+print(find()) 
