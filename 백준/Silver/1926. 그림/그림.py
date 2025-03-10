3079e6eb9e4753528d4b1a29e16385d3b1209d0b
rbflakt <152948102+rbflakt@users.noreply.github.com>
2025-03-10 15:55:42 +0900

    [Silver I] Title: 그림, Time: 368 ms, Memory: 48700 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,7 +1,7 @@
 n,m = map(int,input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 dir1 = [(0,1),(1,0),(-1,0),(0,-1)]
-#dir2 = [(-1,1),(1,-1),(-1,-1),(1,1)]
+dir2 = [(-1,1),(1,-1),(-1,-1),(1,1)]
 def find(graph):
   count = 0
   visited = [[False]*m for _ in range(n)]
@@ -12,26 +12,30 @@ def find(graph):
     area = 1
     while stack:
       cx, cy = stack.pop()
-      neighbors = [(cx + dx, cy + dy) for dx, dy in dir1]  # 상하좌우 위치 계산
-      isolated = all(0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0 for nx, ny in neighbors if 0 <= nx < n and 0 <= ny < m)
+      #if visited[cx][cy]:
+        #continue
+      #visited[cx][cy] = True 이부분 때문에 생각해줘야하는 영역이 전부 패스됨.
       for dx,dy in dir1:
         nx,ny = dx+cx,dy+cy
         if 0 <= nx < n and 0<= ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
-            stack.append((nx,ny))
-            visited[nx][ny] = True
-            area += 1
-        if isolated:
+          stack.append((nx,ny))
+          visited[nx][ny] = True
+          area += 1
+      for dx,dy in dir2:
+        #if 0 <= nx < n and 0<= ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
+          #count += 1
+          #area += 1
+          #stack.append((nx,ny))
+        if 0 <= nx < n and 0<= ny < m and not visited[nx][ny] and graph[nx][ny] == 0:
           result.append(area)
-          return
     result.append(area)
-
   for i in range(n):
     for j in range(m):
       if not visited[i][j] and graph[i][j] == 1:
-        dfs(i,j)
         count += 1
+        dfs(i,j)
   if not result:
-    return count,0
+     return count,0
   return count,max(result)
 a,b = find(graph)
 print(a,b)
