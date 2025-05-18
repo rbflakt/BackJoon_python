4b694ccbe5bcb1915a7dc693495c543a9acab9eb
rbflakt <152948102+rbflakt@users.noreply.github.com>
2025-05-18 19:00:22 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,22 +1,6 @@
 T = int(input())
 for _ in range(T):
-  a = int(input())
-  if a>=25:
-    print(a//25)
-    a = a%25
-  else:
-    print(0)
-  if a>=10:
-      print(a//10)
-      a = a%10
-  else:
-    print(0)
-  if a>=5:
-      print(a//5)
-      a = a%5
-  else:
-    print(0)
-  if a>=1:
-      print(a//1)
-  else:
-    print(0)
+    a = int(input())
+    for unit in [25, 10, 5, 1]:
+        print(a // unit)
+        a %= unit
